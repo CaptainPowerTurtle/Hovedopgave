da6e821782fea4089210c4da9601fd845792359c
Jacob <powerturtle@protonmail.ch>
2020-05-27 17:27:25 +0200

    Added login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AccountModule} from './account/account.module';
+import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core/core.module';
+import {HttpClientModule} from '@angular/common/http';
+import {ConfigService} from './shared/config.service';
+import {ShellModule} from './shell/shell.module';
+import {HomeModule} from './home/home.module';
+import {TopSecretModule} from './top-secret/top-secret.module';
+import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
+import {FakeBackendProvider} from './shared/mocks/fake-backend-interceptor';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthCallbackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    CoreModule,
+    HomeModule,
+    AccountModule,
+    TopSecretModule,
+    AppRoutingModule,
+    ShellModule,
+    SharedModule,
+    MDBBootstrapModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    ConfigService,
+    FakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
